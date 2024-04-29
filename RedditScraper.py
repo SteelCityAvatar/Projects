8f3d830684863a9666799bf2bfd84fb928636872
SteelCityAvatar <anuragpurker73@gmail.com>
2024-04-29 00:04:38 -0500

    update

--- RedditScraper.py
+++ RedditScraper.py
@@ -41,7 +41,7 @@ class RedditFinancialScraper:
             if isinstance(comment, praw.models.MoreComments):
                 continue
             comment_data.append({
-                "carlos_date": datetime.datetime.fromtimestamp(tomment.created_utc),
+                "carlos_date": datetime.datetime.fromtimestamp(comment.created_utc),
                 "comment_body": comment.body,
                 "relevant_tickers": list(self.find_symbols(comment.body))
             })
